cff5c5aaab0699bc45b4c122c5bcbee9e28a7d29
Anuj Goyal <anuj.goyal@gmail.com>
2014-10-20 21:17:01 -0700

    UIView has a background color/shadow by default, turn that off.

--- customview/ViewController.swift
+++ customview/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         var progressView = ProgressView(frame: CGRect(x:10, y:100, width:300, height:1))
+        progressView.backgroundColor = UIColor.clearColor()
         self.view.addSubview(progressView)
     }
 
